bf8742f687c5e1bb34acd088ce78c42717d2747e
obammann <o.bammann@ewetel.net>
2015-11-04 15:53:49 -0800

    Update AssessmentNotification

--- PsychologyApp/app/src/main/java/com/psychology/psychologyapp/Logic/AssessmentNotification.java
+++ PsychologyApp/app/src/main/java/com/psychology/psychologyapp/Logic/AssessmentNotification.java
@@ -11,9 +11,9 @@ import android.support.v7.app.NotificationCompat;
 import android.view.View;
 
 import com.psychology.psychologyapp.Activity.MainActivity;
+import com.psychology.psychologyapp.Fragment.RandomAssessmentFragment;
 import com.psychology.psychologyapp.R;
 
-import java.util.GregorianCalendar;
 
 /**
  * Created by oliverbammann on 03.11.15.
@@ -34,6 +34,7 @@ public class AssessmentNotification {
         mBuilder.setAutoCancel(true);
 
         Intent resultIntent = new Intent(context, MainActivity.class);
+        resultIntent.putExtra("menuFragment", "randomAssessmentFragment");
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
         stackBuilder.addParentStack(MainActivity.class);
         stackBuilder.addNextIntent(resultIntent);
@@ -58,3 +59,51 @@ public class AssessmentNotification {
     }
 
 }
+
+/*
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAssessmentNotification = new AssessmentNotification();
+        //mAssessmentNotification.notificationRandomAssessment(this);
+
+        String menuFragment = getIntent().getStringExtra("menuFragment");
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // If we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+
+            if (menuFragment != null) {
+                if (menuFragment.equals("randomAssessmentFragment")) {
+                    RandomAssessmentFragment randomAssessmentFragment = new RandomAssessmentFragment();
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, randomAssessmentFragment)
+                            .addToBackStack(null)
+                            .commit();
+                }
+            } else {
+                mMainMenuFragment = new MainMenuFragment();
+
+                // Add the fragment to the 'fragment_container' FrameLayout
+                getFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, mMainMenuFragment)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+
+            mAssessmentNotification.alarm(this, findViewById(R.id.fragment_container));
+
+
+
+        }
+
+    }
+ */
